af82edb9a3e6f2eccc7bb754521af450ed189a74
Andrei Z <b1naryth1ef@gmail.com>
2011-05-12 13:35:37 -0500

    Everything now prints to the channel it was called from. Multichannel support is still buggy. Have not yet found a fix for #1.
    Signed-off-by: Andrei Z <b1naryth1ef@gmail.com>

--- botty.py
+++ botty.py
@@ -1,11 +1,11 @@
-import socket, time, sys
+import socket, time, sys, urllib
 from datetime import date
 from modules import tweety
-from configobj import ConfigObj
 
 __Version__ = "0.0.1"
 __Author__  = "B1naryth1ef"
 
+chans = ["#wocurt","#libraryofgames","#anapnea2","#anapnea"] #Channels the bot is allowed in
 admins = [":B1naryth1ef", ":HarryD"]
 network = 'irc.freenode.net'
 channel = '#wocurt'
@@ -15,9 +15,23 @@ cs = "HfxlAXPN9Oz1rVIzXcfOWVmtxYHWGAEP4K1GVNcdY"
 ak = "25340639-iWxKLUrtZ6Vao2JcRHCqcw1NBIlBMUGwcbcKC1pWx"
 asx = "zr5iWv9PKHFnXcbhvWb65ZNdyqAK9HB8noGoJTk64"
 
+#CHALO::#wocurt
+#I[2]:#wocurt
+#INFO
+#[0]
+#[1]B1naryth1ef!~b1naryth1@adsl-68-255-109-4.dsl.chcgil.ameritech.net PRIVMSG #wocurt 
+#[2]!test
+#
+#I
+#[0]B1naryth1ef!~b1naryth1@adsl-68-255-109-4.dsl.chcgil.ameritech.net
+#[1]PRIVMSG
+#[2]#wocurt
+#[3]
+
 def sendm(msg):
     irc.send('PRIVMSG '+channel+' :'+str(msg)+'\r\n')
-
+def sendc(msg,chanc):
+    irc.send('PRIVMSG '+chanc+' :'+str(msg)+'\r\n')
 port = 6667
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect ((network, port))
@@ -33,7 +47,7 @@ wiki = None
 tweet1 = None
 tweet2 = None
 
-def sender(input):
+def sender(input): #UPDATE NEEDED
 	xinput = input.split("!"); print xinput[0];return xinput[0]
 
 def voice(input):
@@ -41,34 +55,34 @@ def voice(input):
 	voices= voice[1].strip()
 	irc.send('MODE '+str(channel) +' +v '+str(voices) +'\r\n')
 
-def wiki(info):
+def wiki(info,i): #Updated
 	wiki = info.split(":!wiki")
 	wikis=wiki[1].strip()
 	if len(wikis) < 1:
-		sendm('[+] Error: Example : !wiki Britney')
+		sendc('[+] Error: Example : !wiki Britney',i[2])
 	else:
-		sendm('[+] Wiki: : http://www.wikipedia.org/wiki/'+str(wikis))
+		sendc('[+] Wiki: : http://www.wikipedia.org/wiki/'+str(wikis),i[2])
 
-def timey(info):
+def timey(info,i): #updated
 	if info.find (':!time') != -1:
-		sendm('[+]  Time: '+time.strftime("%H:%M:%S", time.localtime()))
+		sendc('[+]  Time: '+time.strftime("%H:%M:%S", time.localtime()),i[2])
 
-def date(info):
+def date(info,i): #updated
 	if info.find (':!date') != -1:
-		sendm('[+]  Date: '+time.strftime("%a, %b %d, %y", time.localtime()))
+		sendc('[+]  Date: '+time.strftime("%a, %b %d, %y", time.localtime()),i[2])
 
-def twitterbot(info):
+def twitterbot(info,i): #updated
     try:
 		tweet1 = info.split(":!twitter")
 		tweet2 = tweet1[1].strip()
 		if len(tweet2) < 1:
-			sendm('[+] Error: Twitter Example: !twitter USERNAME') 
+			sendc('[+] Error: Twitter Example: !twitter USERNAME',i[2]) 
 		else:
 			x = tweety.twitterx(tweet2,ck,cs,ak,asx)
 			x2 = "[+]",x[0]
-			sendm(x)  
+			sendc(x,i[2])  
     except:
-        sendm('[+] Error: Not a valid entry. Twitter Example: !twitter USERNAME')       
+        sendc('[+] Error: Not a valid entry. Twitter Example: !twitter USERNAME',i[2])       
 def botjoin(info):
     x = sender(info)
     if x in admins:
@@ -108,63 +122,86 @@ def botop(info):
     else:
         sendm('Must be admin to use !bot commands')
 
-def weather(info):
-    sendm(">>>>>>>  404  <<<<<<<")
-    sendm("Weather is still WIP. :D")
-def webpage(info):
-    #This will detect for links (http://) and then get the title of them using urllib2
-    pass
-def wolfram(info):
+def weather(info,i):
+    sendc(">>>>>>>  404  <<<<<<<",i[2])
+    sendc("Weather is still WIP. :D",i[2])
+def webpage(info,i): 
+    info2=info.split(":",2)
+    uri = str(info2[2])
+    print "1"
+    u = urllib.urlopen(uri)
+    print "2"
+    infox = u.info()
+    u.close()
+    print "3"
+    msg = "Header for "+info2[2]+" ["+infox+" ]"
+    print i[2]
+    sendc(msg,i[2])
+
+def wolfram(info,i):
     iz = info.split(" ")
     url1 = "http://www.wolframalpha.com/input/?i="
     url2 = url1+iz[4]
     url3 = str(url2)+str("]")
     msg = "Wolfram Alpha ["+url3+url3
     msg2 = msg
-    sendm(msg2)
+    sendc(msg2,i[2])
 
 
-while True:
+while True:    
     info = irc.recv(4096)
     print info
-    if info.find(':!twitter') != -1:
-    	twitterbot(info)
+    info2=info.split(":",2) 
+    i = info2[1].split(" ")
+    chalo = str(channel)
+    try:
+        if i[2] in chans:
+            if info.find(':!test') != -1:
+                if i[2] == chalo:
+                    print "WE HAVE A WINNER!"
+                else:
+                    pass
+            if info.find(':!twitter') != -1:
+            	twitterbot(info,i)
 
-    if info.find('PING') !=-1:
-        info.split(" ",1)
-    	senderx = "PONG "+info[1]+"\n"
-        print senderx
-        f = str(senderx)
-    	irc.send(f)
+            if info.find('PING') !=-1:
+                info.split(" ",1)
+                senderx = "PONG "+info[1]+"\n"
+                print senderx
+                f = str(senderx)
+                irc.send(f)
 
-    if info.find(':!voice') != -1:
-    	voice(info)
+            if info.find(':!voice') != -1:
+            	voice(info)
 
-    if info.find (':!wiki') != -1:
-    	wiki(info)
+            if info.find (':!wiki') != -1:
+            	wiki(info,i)
 
-    if info.find(':!time') != -1:
-    	timey(info)
+            if info.find(':!time') != -1:
+            	timey(info,i)
 
-    if info.find(':!test') != -1:
-    	sender(info)
+            if info.find(':!date') != -1:
+            	date(info, i)
 
-    if info.find(':!date') != -1:
-    	date(info)
+            if info.find (':!bot quit') != -1:
+            	botquit(info)
 
-    if info.find ('!bot quit') != -1:
-    	botquit(info)
+            if info.find(':!bot join') != -1:
+                botjoin(info)
 
-    if info.find('!bot join') != -1:
-        botjoin(info)
+            if info.find(':!bot part') != -1:
+                botpart(info)
 
-    if info.find('!bot part') != -1:
-        botpart(info)
+            if info.find(':!wolf') != -1:
+                wolfram(info,i)
+                   
+            if info.find(':!weather') !=-1:
+                weather(info,i)
 
-    if info.find('!wolf') != -1:
-        wolfram(info)
-           
-    if info.find('!weather') !=-1:
-        weather(info)
-    if info.find('!bot op') !=-1:
-        botop(info)
+            if info.find(':!bot op') !=-1:
+                botop(info)
+            if "http" in info:
+                print "HERE"
+                webpage(info,i)
+    except:
+        pass
